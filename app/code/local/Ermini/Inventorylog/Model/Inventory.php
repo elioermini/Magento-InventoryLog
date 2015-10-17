7bbc62ecca3fbc37d4bd18127e007149b3092b36
Elio Ermini <elio.ermini@gmail.com>
2015-10-17 16:11:01 +0200

    Refactoring of the Observer. Now it listens sales_model_service_quote_submit_success

--- app/code/local/Ermini/Inventorylog/Model/Inventory.php
+++ app/code/local/Ermini/Inventorylog/Model/Inventory.php
@@ -8,6 +8,8 @@
  */
 
 /**
+ * Inventory model
+ *
  * Class Ermini_Inventorylog_Model_Inventory is in charge of logging the stock movements of product
  *
  * @method getData()
@@ -17,37 +19,39 @@ class Ermini_Inventorylog_Model_Inventory extends Mage_Core_Model_Abstract
 {
 
     /**
+     * @_construct Ermini_Inventorylog_Model_Inventory
+     */
+    protected function _construct()
+    {
+        $this->_init('inventorylog/inventory');
+    }
+
+    /**
      * Inserts stock log in table of Ermini_Inventorylog_Model_Inventory
      *
-     * @param $item - the product that changed stock
+     * @param $item Mage_Catalog_Model_Product - the product that changed stock
      * @param $message
      *
      */
     public function insertStockLog($item, $message)
     {
         $helper = Mage::helper('inventorylog');
+        $id=$item->getId();
         $originalQty = (int)$item->getOrigData('qty');
         $newQty = (int)$item->getData('qty');
         $difference = $newQty - $originalQty;
+        $stock=Mage::getModel('cataloginventory/stock_item')->loadByProduct($id)->getIsInStock();
 
-        $this->setItemId($item->getProductId())
+        $this->setItemId($id)
             ->setUser($helper->_getUsername())
             ->setUserId($helper->_getUserId())
             ->setIsAdmin((int)Mage::getSingleton('admin/session')->isLoggedIn())
             ->setMovement($difference)
             ->setQty($newQty)
-            ->setIsInStock((int)$item->getIsInStock())
+            ->setIsInStock($stock)
             ->setMessage($message)
             ->setCreatedAt($helper->getDate())
             ->save();
-    }
-
-    /**
-     * @_construct Ermini_Inventorylog_Model_Inventory
-     */
-    protected function _construct()
-    {
-        $this->_init('inventorylog/inventory');
     }
 
 }
--- app/code/local/Ermini/Inventorylog/Model/Observer.php
+++ app/code/local/Ermini/Inventorylog/Model/Observer.php
@@ -10,17 +10,23 @@
 /**
  * Class Ermini_Inventorylog_Model_Observer will catch events fired by Magento
  *
+ * @method Mage_Catalog_Model_Product getStockStatusChangedAutomaticallyFlag()
+ * @method Mage_Catalog_Model_Product getOriginalInventoryQty()
+ * @method
+ *
  *
  */
 class Ermini_Inventorylog_Model_Observer
 {
-
     /**
      * Logs changes when manually done with Api or Admin user
      * @param Varien_Event_Observer $observer
      */
     public function saveInventoryAfter(Varien_Event_Observer $observer)
     {
+        /**
+         * @var $product Mage_Catalog_Model_Product
+         */
         $product = $observer->getEvent()->getItem();
         if (!$product->getStockStatusChangedAutomaticallyFlag() || $product->getOriginalInventoryQty() != $product->getQty()) {
             if (!$message = $product->getSaveMovementMessage()) {
@@ -30,9 +36,19 @@ class Ermini_Inventorylog_Model_Observer
                     $message = 'Stock saved manually';
                 }
             }
-            Mage::getModel('inventorylog/inventory')->insertStockLog($product, $message);
+            $this->_getInventoryModel()->insertStockLog($product, $message);
         }
 
+    }
+
+
+    /**
+     * @return Ermini_Inventorylog_Model_Inventory
+     */
+    private function _getInventoryModel()
+    {
+
+        return Mage::getModel('inventorylog/inventory');
     }
 
     /**
@@ -43,29 +59,55 @@ class Ermini_Inventorylog_Model_Observer
     {
 
         $quote = $observer->getEvent()->getQuote();
+        $message = 'Checkout';
+        $helper = Mage::helper('inventorylog');
+
+        /**
+         * @var $quote Mage_Sales_Model_Quote
+         * loops through the items being purchased
+         *
+         * @var $item Mage_Sales_Model_Quote_Item
+         */
         foreach ($quote->getAllItems() as $item) {
 
-            $message = 'Checkout';
 
-            $params = array();
-            $params['item_id'] = $item->getProductId();
-            $params['user'] = 'Customer';
-            $params['user_id'] = '';
-            $params['qty'] = $item->getProduct()->getStockItem()->getQty();
-            $params['movement'] = ($item->getTotalQty() * -1);
-            $params['is_in_stock'] = '';
-            $params['created_at'] = Mage::helper('inventorylog')->getDate();
-            Mage::getModel('inventorylog/inventory')->setItemId($item->getProductId())
-                ->setUser('Customer')
-                ->setUserId('')
-                ->setIsAdmin('No')
-                ->setMovement($params['movement'])
-                ->setQty($params['qty'])
-                ->setIsInStock('')
-                ->setMessage($message)
-                ->setCreatedAt($params['created_at'])
-                ->save();
+            $id = $item->getProductId();
+            /**
+             * @var $currentQty int is the stock of product before order
+             */
+
+            $stockItem = Mage::getModel('cataloginventory/stock_item')->loadByProduct($id);
+            /**
+             * @var $qty int is the stock of product before order
+             */
+            $qty = $stockItem->getQty();
+            /**
+             * @var $stock bool|int tells if the product is in stock or not
+             */
+            $stock = $stockItem->getIsInStock();
+            /**
+             * @var $purchasedQty int is the quantity being purchased for this product
+             */
+            $purchasedQty = (int)$item->getTotalQty();
+            /**
+             * @var $movement int is the change between the previous quantity and the quantity being purchased
+             *      will be the negation of the absolute number
+             */
+            $movement = ($purchasedQty * -1);
+
+            $model = $this->_getInventoryModel();
+
+            $model->setMovement($movement);
+            $model->setItemId($id);
+            $model->setUser($helper->_getUsername());
+            $model->setUserId($helper->_getUserId());
+            $model->setIsAdmin((int)Mage::getSingleton('admin/session')->isLoggedIn());
+            $model->setQty($qty);
+            $model->setIsInStock($stock);
+            $model->setMessage($message);
+            $model->setCreatedAt(Mage::helper('inventorylog')->getDate());
+            $model->save();
+
         }
-
     }
 }
